6f50bb72e67938f4de8d65ffe9d31b403c921177
Umidjonn <bolbekovumid1001@gmail.com>
2023-03-24 02:18:20 +0500

    add torus and remove axes helper

--- src/script.js
+++ src/script.js
@@ -18,13 +18,14 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 
-// Axes helper
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
+// // Axes helper
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('./textures/matcaps/8.png')
 
 /**
  * Fonts
@@ -60,12 +61,29 @@ fontLoader.load(
         //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5
         // )
         textGeometry.center()
-        const textMaterial = new THREE.MeshBasicMaterial({ 
-                                                            wireframe:true,
-                                                            color: 'white'
-                                                               })
+        
+        const textMaterial = new THREE.MeshMatcapMaterial({ matcap:matcapTexture})
         const text = new THREE.Mesh(textGeometry, textMaterial)
         scene.add(text)
+
+       
+        for(let i = 0; i < 100; i++)
+{
+    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+    const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+    const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+    scene.add(donut)
+    donut.position.x = (Math.random() - 0.5) * 10
+    donut.position.y = (Math.random() - 0.5) * 10
+    donut.position.z = (Math.random() - 0.5) * 10
+
+    donut.rotation.x = Math.random() * Math.PI
+    donut.rotation.y = Math.random() * Math.PI
+
+    const scale = Math.random()
+    donut.scale.set(scale, scale, scale)
+}
+
     }
 )
 
